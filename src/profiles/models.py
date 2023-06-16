04195951c8cc41268ae90ef3f26d4a5b04579252
BobHawler <102181473+BobHawler@users.noreply.github.com>
2023-06-16 11:36:43 +0600

    Adding Comments and Likes models and methods

--- src/profiles/models.py
+++ src/profiles/models.py
@@ -23,6 +23,27 @@ class Profile(models.Model):
     def get_friends_no(self):
         return self.friends.all().count()
 
+    def get_posts_no(self):
+        return self.posts.all().count()
+
+    def get_all_authors_posts(self):
+        return self.posts.all()
+
+    def get_likes_given_no(self):
+        likes = self.like_set.all()
+        total_liked = 0
+        for item in likes:
+            if item.value == 'Like':
+                total_liked += 1
+        return total_liked
+
+    def get_likes_recieved_no(self):
+        posts = self.posts.all()
+        total_liked = 0
+        for item in posts:
+            total_liked += item.liked.all().count()
+        return total_liked
+
     def __str__(self):
         return f"{self.user.username}-{self.created.strftime('%d-%m-%Y')}"
 
@@ -47,8 +68,10 @@ STATUS_CHOISES = (
 
 
 class Relationship(models.Model):
-    sender = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='sender')
-    receiver = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='receiverr')
+    sender = models.ForeignKey(Profile, on_delete=models.CASCADE,
+                               related_name='sender')
+    receiver = models.ForeignKey(Profile, on_delete=models.CASCADE,
+                                 related_name='receiverr')
     status = models.CharField(max_length=8, choices=STATUS_CHOISES)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
